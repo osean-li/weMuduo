5c8e24e38dd47a246a319f278d03c20d2b9a0fc8
osean-li <lht1205613211@163.com>
2021-07-11 19:01:02 +0800

    add tcpserver

--- Channel.cpp
+++ Channel.cpp
@@ -32,13 +32,13 @@ void Channel::tie(const std::shared_ptr<void> &obj)
 void Channel::update()
 {
 	// 通过channel所属的EventLoop，调用poller的相应方法，注册fd的events事件
-	//loop_->updateChannel(this);
+	loop_->updateChannel(this);
 }
 
 // 在channel所属的EventLoop中， 把当前的channel删除掉
 void Channel::remove()
 {
-	//loop_->removeChannel(this);
+	loop_->removeChannel(this);
 }
 
 // fd得到poller通知以后，处理事件的
